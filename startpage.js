f9bdbe1c28b8a2c8a881696a72400e20e3e8f615
ZuggRugg <shayne.tman@gmail.com>
2025-06-26 11:06:22 -0500

    fixed repo, hopefully

--- startpage.js
+++ startpage.js
@@ -1,21 +1,31 @@
+main();
+
+
+
+function main() {
 getTime();
 fetchData(97, 78);
+
+}
+
+
 
 async function fetchData(X, Y) {
     try {
 	const response1 = await fetch("https://api.weather.gov/gridpoints/LSX/" + X + "," + Y + "/forecast/hourly");
-        const response2 = await fetch("https://api.weather.gov/gridpoints/LSX/" + X + "," + Y + "/forecast");
+       const response2 = await fetch("https://api.weather.gov/gridpoints/LSX/" + X + "," + Y + "/forecast");
+
         if((!response1.ok) && (!response2.ok)){throw new Error("Could not fetch response's");}
- 
-        const data1 = await response1.json();
+	
+       const data1 = await response1.json();
 	const data2 = await response2.json();
 	if(X == 97 && Y == 78)
- {document.getElementById('location').innerHTML = "Edwardsville, Illinois"; const container = document.getElementById('scrollmenu');
-  container.textContent = '';}
-       
+	{document.getElementById('location').innerHTML = "Edwardsville, Illinois"; const container = document.getElementById('scrollmenu');
+	 container.textContent = '';}
+	
 	var jsondata1 = data1; 
         var jsondata2 = data2;
-//	console.log(jsondata1, jsondata2); 
+	//console.log(jsondata1, jsondata2); 
 	getWeather(jsondata1,jsondata2);
     }
     catch(error){console.error(error);}
@@ -24,12 +34,12 @@ async function fetchData(X, Y) {
 
 function getWeather(jsondata1, jsondata2) {
     var temp = document.getElementById("temp");
-        temp.innerHTML = jsondata1.properties.periods[0].temperature + jsondata1.properties.periods[0].temperatureUnit;
+    temp.innerHTML = jsondata1.properties.periods[0].temperature + jsondata1.properties.periods[0].temperatureUnit;
     
     var a = document.getElementById('main_img'); 
 
-        scrollmenu(jsondata1,jsondata2);
-        a.src = determineIcon(jsondata1, 0);
+    scrollmenu(jsondata1,jsondata2);
+    a.src = determineIcon(jsondata1, 0);
 }
 
 
@@ -40,16 +50,57 @@ function determineIcon(jsondata1, t) {
     var iconN = iconlink.slice(35, 42);
     console.log(iconN, iconlink); 
 
-    if(iconN == "night/f") {if(t==0){determineBackground("night");}return "img/clear_night.png";}
-    else if((iconN == "day/skc") || (iconN == "day/few") || (iconN == "day/hot")) {if(t==0){determineBackground("day");} return "img/Sunny.png";}
-    else if((iconN == "day/sct") || (iconN == "day/bkn") || (iconN == "day/ovc")) {if(t==0){determineBackground("cloud");} return "img/cloudy_weather.png";}
-    else if(iconN == "day/rai") {if(t == 0){determineBackground("cloud");} return "img/rainy.png";} 
-    else if((iconN == "night/s") || (iconN == "night/b")) {if(t == 0){determineBackground("night");} return "img/cloudy_night.png";}
-    else if(iconN == "day/tsr") {return "img/thunderstorm.png";}
-    else if(iconN == "night/t") {return "img/nThunder.png"}
-    else if((iconN == "night/b") || (iconN == "night/o")) {if(t==0){determineBackground("night");} return "img/day_windy.png"}
-    else if(iconN == "night/r") {if(t==0){determineBackground("night");} return "img/rain_night.png";}
+
+    var code_arr = ["night/f", "day/skc", "day/sct", "day/few", "day/hot", "day/bkn",
+			       "day/ovc", "day/rai", "night/s", "night/b", "day/tsr", "night/t", "night/b", "night/o", "night/r"];
+
+    var icon_arr = ["img/clear_night.png", "img/Sunny.png", "img/cloudy_weather.png", "img/rainy.png",
+		           "img/cloudy_night.png", "img/thunderstorm.png", "img/nThunder.png", "img/rain_night.png"];
+ 
+
+    if(iconN == code_arr[0]) {
+	if(t==0){determineBackground("night");}
+	return icon_arr[0];
+    }
+
+    else if((iconN == code_arr[1]) || (iconN == code_arr[3]) || (iconN == code_arr[4])) {
+	if(t==0){determineBackground("day");} 
+	return icon_arr[1];
+    }
+
+    else if((iconN == code_arr[2]) || (iconN == "day/bkn") || (iconN == "day/ovc")) {
+	if(t==0){determineBackground("cloud");} 
+	return icon_arr[2];}
+
+    else if(iconN == "day/rai") {
+	if(t == 0){determineBackground("cloud");}
+	return icon_arr[3];
+    } 
+
+    else if((iconN == "night/s") || (iconN == "night/b")) {
+	if(t == 0){determineBackground("night");} 
+	return icon_arr[4];
+    }
+
+    else if(iconN == "day/tsr") {
+	return icon_arr[5];
+    }
+
+    else if(iconN == "night/t") {
+	return icon_arr[6];
+    }
+
+    else if((iconN == "night/b") || (iconN == "night/o")) {
+	if(t==0){determineBackground("night");} 
+	return "img/day_windy.png";
+    }
+
+    else if(iconN == "night/r") {
+	if(t==0){determineBackground("night");} 
+	return "img/rain_night.png";
+    }
 }
+
 
 async function SearchBox() {
     document.getElementById("scrollmenu").innerHTML = "";
@@ -90,29 +141,28 @@ const gridpoints = "https://api.weather.gov/points/" + lat + "," + lon;
 
 async function getlinks(passed) {    
     var name = document.getElementById("redsearch").value;
-    if(passed == "Tech") {var url = ["https://www.reddit.com/r/tech/hot.json", "https://www.reddit.com/r/programming/hot.json"];}
-    else if(passed == "news") {var url = ["https://old.reddit.com/r/news/hot.json","https://old.reddit.com/r/worldnews/hot.json" ];}
-   else {var link = "https://www.reddit.com/r/" + name + "/hot.json"; var url = [link];}
+    if(passed == "news") {var url = ["https://old.reddit.com/r/news/hot.json","https://old.reddit.com/r/worldnews/hot.json" ];}
+    else if(passed == "Politic") {var url = ["https://old.reddit.com/r/Destiny/hot.json", "https://old.reddit.com/r/slatestarcodex/hot.json"]}
+    else if(passed == "Code") {var url = ["https://old.reddit.com/r/Emacs/hot.json", "https://old.reddit.com/r/Programming/hot.json"]}
+    else {var link = "https://www.reddit.com/r/" + name + "/hot.json"; var url = [link];}
 
     try {
 	for(let i = 0; i < url.length; i++) {
 	    const linkres = await fetch(url[i]);
 	    if(!linkres.ok) {throw new Error("could not fetch response");}
 	    const linkdata = await linkres.json();
-//	    console.log(linkdata);
+	    console.log(linkdata);
 	    appendlinks(linkdata);
 	}
     }
 	catch(error){console.error(error)};
 }
 
-
-
 function appendlinks(linkdata) {
     const names = document.getElementById("links");
     names.appendChild(document.createElement("br"));
 
-    for(var j = 3; j < 13; j++) {
+    for(var j = 3; j < 16; j++) {
     var a = document.createElement('a');
     var title = linkdata.data.children[j].data.title;
     var linkText = document.createTextNode(title);
@@ -163,9 +213,13 @@ function scrollmenu(jsondata1, jsondata2){
 }
 
 function determineBackground(code) {
-    if(code == "day") {document.body.style.backgroundImage = "linear-gradient(224deg, #EBFF00FF 0.5%, #EBFF00FF 1%, #71C4FFFF 40%)"}
-    else if(code == "night") {document.body.style.backgroundImage = "linear-gradient(224deg, #663399 1%, #663399 1%, #006db0 80%)"}
-    else if(code == "cloud") {document.body.style.backgroundImage = "linear-gradient(224deg, #ffffff 2%, #ffffff 5%, #99ccff 40%)"}
+    day = "linear-gradient(224deg, #EBFF00FF 0.5%, #EBFF00FF 1%, #71C4FFFF 40%)";
+    night = "linear-gradient(224deg, #663399 1%, #663399 1%, #006db0 80%)";
+    cloud = "linear-gradient(224deg, #ffffff 2%, #ffffff 5%, #99ccff 40%)"; 
+
+    if(code == "day") {document.body.style.backgroundImage = day;}
+    else if(code == "night") {document.body.style.backgroundImage = night;}
+    else if(code == "cloud") {document.body.style.backgroundImage = cloud;}
 }
 
 
