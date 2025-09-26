055acbe807af44d4e7ffb4ab9d52f6fe445b60f0
ZuggRugg <155196761+ZuggRugg@users.noreply.github.com>
2025-09-26 10:38:53 -0500

    Update startpage.js

--- startpage.js
+++ startpage.js
@@ -9,9 +9,7 @@ async function fetchData(X, Y) {
  
         const data1 = await response1.json();
 	const data2 = await response2.json();
-	if(X == 97 && Y == 78)
- {document.getElementById('location').innerHTML = "Edwardsville, Illinois"; const container = document.getElementById('scrollmenu');
-  container.textContent = '';}
+	
        
 	var jsondata1 = data1; 
         var jsondata2 = data2;
@@ -92,7 +90,6 @@ const gridpoints = "https://api.weather.gov/points/" + lat + "," + lon;
 async function getlinks(passed) {    
     var name = document.getElementById("redsearch").value;
     if(passed == "news") {var url = ["https://old.reddit.com/r/news/hot.json","https://old.reddit.com/r/worldnews/hot.json" ];}
-    else if(passed == "Politic") {var url = ["https://old.reddit.com/r/Destiny/hot.json", "https://old.reddit.com/r/slatestarcodex/hot.json"]}
     else if(passed == "Code") {var url = ["https://old.reddit.com/r/Emacs/hot.json", "https://old.reddit.com/r/Programming/hot.json"]}
        else {var link = "https://www.reddit.com/r/" + name + "/hot.json"; var url = [link];}
 
